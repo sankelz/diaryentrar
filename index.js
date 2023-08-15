0e722589a9857d4116de6a02219d6f8a8bfd70f7
eshi s <122346648+sankelz@users.noreply.github.com>
2023-08-14 20:11:00 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+class Diary{
+  constructor(date, entry){
+    this.date = date
+    this.entry = entry 
+  }
+}
+
+date = window.prompt("today is: ")
+entry =window.prompt("and i feel like: ")
+
+dintry = new Diary(date, entry)
+
+console.log(diaryentry)
